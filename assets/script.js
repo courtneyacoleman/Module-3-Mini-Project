3140a4b7f2aa83d7c925051d1e57b68ea713a742
Courtney Coleman <105394516+courtneyacoleman@users.noreply.github.com>
2022-07-08 19:15:50 -0500

    needs local storage

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,62 @@
-document.getElementById("startButton").addEventListener('click', playerChooses);
+let player = '';
+document.getElementById('startButton').addEventListener('click', playerChooses);
+
 
 function playerChooses() {
-  prompt ('Rock, Paper, or Scissors? Enter R, P, or S');
+  let playerInput = prompt('Rock, Paper, or Scissors? Enter R, P, or S');
+  player = playerInput.toUpperCase();
+  console.log('playerInput', playerInput);
+  console.log("player:", player);  
+if ((player === 'R') || (player ==='P') || (player==='S')) {
+  compChooses ();
 }
+else {
+  alert('Choose a valid weapon!');
+}
+}
+
+function compChooses() {
+  let compOptions=['R', 'P', 'S'];
+  let rando = Math.floor(Math.random() * compOptions.length);
+  let computer = compOptions[rando]
+  console.log("compChooses:", computer);
+  alert ("Computer chooses " + computer)
+
+
+if (player === computer) {
+  alert('Tie game!');
+  stats.ties++;
+}
+else if ((player === 'R' && computer === 'S') || (player=== 'P' && computer === 'R') || (player ==='S' && computer ==='P')) {
+  alert('Player wins');
+  stats.wins++
+}
+else {
+  alert('Computer wins!');
+  stats.losses++
+}
+console.log(stats);
+createStats();
+readValue();
+}
+
+const stats = {
+  wins: 0,
+  losses: 0,
+  ties: 0,
+}
+ 
+function createStats() {
+  localStorage.setItem('wins', stats.wins);
+  localStorage.setItem('losses', stats.losses);
+  localStorage.setItem('ties', stats.ties);
+}
+
+function readValue() {
+    var x = localStorage.getItem("mytime");
+    document.getElementById("stats").innerHTML = x;
+    var y = localStorage.getItem("losses");
+    document.getElementById("stats").innerHTML = y;
+    var z = localStorage.getItem("ties");
+    document.getElementById("stats").innerHTML = z;
+  }
